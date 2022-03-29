c65d15a9658287691e1ee52e7ff627ad4f601aac
Nuno Vieira <nuno.fcvieira93@gmail.com>
2022-03-29 03:13:03 +0100

    Add Observable<Value>
    This will be the observable pattern to be used by the presentation layer to subscribe to view model changes

---
+++ GistClient/Presentation/Common/Observable.swift
@@ -0,0 +1,38 @@
+// Copyright © 2022 nuno-vieira. All rights reserved.
+
+import Foundation
+
+public final class Observable<Value> {
+
+    struct Observer<Value> {
+        weak var observer: AnyObject?
+        let onChange: (Value) -> Void
+    }
+
+    private var observers = [Observer<Value>]()
+
+    public var value: Value {
+        didSet { notifyObservers() }
+    }
+
+    public init(_ value: Value) {
+        self.value = value
+    }
+
+    public func observe(_ observer: AnyObject, onChange: @escaping (Value) -> Void) {
+        observers.append(Observer(observer: observer, onChange: onChange))
+        onChange(self.value)
+    }
+
+    public func remove(_ observer: AnyObject) {
+        observers = observers.filter { $0.observer !== observer }
+    }
+
+    private func notifyObservers() {
+        for observer in observers {
+            DispatchQueue.main.async {
+                observer.onChange(self.value)
+            }
+        }
+    }
+}
